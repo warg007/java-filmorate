77c4d7b12c2d22473747bc4aba077295be695dbc
Warg <warg007@mail.ru>
2023-05-24 19:54:49 +0300

    fixed syntax again

--- src/test/java/ru/yandex/practicum/filmorate/Controllers/FilmControllerTest.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.Controllers;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class FilmControllerTest {
-
-}
--- src/test/java/ru/yandex/practicum/filmorate/Controllers/UserControllerTest.java
+++
@@ -1,9 +0,0 @@
-package ru.yandex.practicum.filmorate.Controllers;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class UserControllerTest {
-
-}
