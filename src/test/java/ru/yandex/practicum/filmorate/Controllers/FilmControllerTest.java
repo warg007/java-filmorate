f85da0a74b43f766ec4242b7d9b11250a622bafa
Warg <warg007@mail.ru>
2023-05-26 21:01:26 +0300

    fixed .

--- src/test/java/ru/yandex/practicum/filmorate/Controllers/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/Controllers/FilmControllerTest.java
@@ -12,63 +12,63 @@ class FilmControllerTest {
 
     FilmController filmController;
 
-    Film filmOk1 = Film.builder().
-            name("film1").
-            id(1).
-            description("film1").
-            releaseDate(LocalDate.of(2000, 10, 10)).
-            duration(10).
-            build();
+    Film filmOk1 = Film.builder()
+            .name("film1")
+            .id(1)
+            .description("film1")
+            .releaseDate(LocalDate.of(2000, 10, 10))
+            .duration(10)
+            .build();
 
-    Film filmOk2 = Film.builder().
-            name("film2").
-            id(2).
-            description("film2").
-            releaseDate(LocalDate.of(2020, 10, 10)).
-            duration(10).
-            build();
+    Film filmOk2 = Film.builder()
+            .name("film2")
+            .id(2)
+            .description("film2")
+            .releaseDate(LocalDate.of(2020, 10, 10))
+            .duration(10)
+            .build();
 
-    Film filmWithoutName = Film.builder().
-            name(" ").
-            id(3).
-            description("film3").
-            releaseDate(LocalDate.of(2020, 10, 10)).
-            duration(10).
-            build();
+    Film filmWithoutName = Film.builder()
+            .name(" ")
+            .id(3)
+            .description("film3")
+            .releaseDate(LocalDate.of(2020, 10, 10))
+            .duration(10)
+            .build();
 
-    Film filmDescriptionLenghtMore200 = Film.builder().
-            name("film4").
-            id(4).
-            description("film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4" +
+    Film filmDescriptionLenghtMore200 = Film.builder()
+            .name("film4")
+            .id(4)
+            .description("film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4" +
                     "film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4" +
-                    "film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4").
-            releaseDate(LocalDate.of(2020, 10, 10)).
-            duration(10).
-            build();
+                    "film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4film4")
+            .releaseDate(LocalDate.of(2020, 10, 10))
+            .duration(10)
+            .build();
 
-    Film filmWithWrongData1 = Film.builder().
-            name("film5").
-            id(5).
-            description("film5").
-            releaseDate(LocalDate.of(2222, 10, 10)).
-            duration(10).
-            build();
+    Film filmWithWrongData1 = Film.builder()
+            .name("film5")
+            .id(5)
+            .description("film5")
+            .releaseDate(LocalDate.of(2222, 10, 10))
+            .duration(10)
+            .build();
 
-    Film filmWithWrongData2 = Film.builder().
-            name("film6").
-            id(6).
-            description("film5").
-            releaseDate(LocalDate.of(1, 10, 10)).
-            duration(10).
-            build();
+    Film filmWithWrongData2 = Film.builder()
+            .name("film6")
+            .id(6)
+            .description("film5")
+            .releaseDate(LocalDate.of(1, 10, 10))
+            .duration(10)
+            .build();
 
-    Film filmWithWrongDuration = Film.builder().
-            name("film7").
-            id(7).
-            description("film7").
-            releaseDate(LocalDate.of(2000, 10, 10)).
-            duration(-10).
-            build();
+    Film filmWithWrongDuration = Film.builder()
+            .name("film7")
+            .id(7)
+            .description("film7")
+            .releaseDate(LocalDate.of(2000, 10, 10))
+            .duration(-10)
+            .build();
 
     @BeforeEach
     void clearController() {
