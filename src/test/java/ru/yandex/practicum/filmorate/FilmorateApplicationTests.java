5af006c1022986b12d04581a46b74984def6e07c
Warg <warg007@mail.ru>
2023-05-26 21:03:57 +0300

    fixed

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
+@SpringBootTest
 class FilmorateApplicationTests {
 
 	@Test
