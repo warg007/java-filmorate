1129a6385a84503c4f7e52833a1e53dd4332a3fb
Warg007 <romzik007@yandex.ru>
2023-07-08 16:13:55 +0300

    without tests

--- src/main/java/ru/yandex/practicum/filmorate/Exceptions/DataNotExistsException.java
+++ src/main/java/ru/yandex/practicum/filmorate/Exceptions/DataNotExistsException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.Exceptions;
 
-public class DataNotExistsException extends RuntimeException{
+public class DataNotExistsException extends RuntimeException {
 
     public DataNotExistsException(String message) {
         super(message);
