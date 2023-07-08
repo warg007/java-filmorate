12f6de5f373223a362605c18d7d60348ed58a919
Warg007 <romzik007@yandex.ru>
2023-07-08 17:04:43 +0300

    without tests

--- src/test/java/ru/yandex/practicum/FilmoRateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/FilmoRateApplicationTests.java
@@ -1,34 +1,34 @@
-package ru.yandex.practicum;
-
-import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
-import ru.yandex.practicum.filmorate.Model.User;
-import ru.yandex.practicum.filmorate.Storage.UserDbStorage;
-
-import java.util.Optional;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-@SpringBootTest(classes = UserDbStorage.class)
-@AutoConfigureTestDatabase
-@RequiredArgsConstructor(onConstructor_ = @Autowired)
-public class FilmoRateApplicationTests {
-    private final JdbcTemplate jdbcTemplate = new JdbcTemplate();
-    private final UserDbStorage userStorage = new UserDbStorage(jdbcTemplate);
-
-    @Test
-    public void testFindUserById() {
-
-        Optional<User> userOptional = userStorage.getUserById(1);
-
-        assertThat(userOptional)
-                .isPresent()
-                .hasValueSatisfying(user ->
-                        assertThat(user).hasFieldOrPropertyWithValue("id", 1)
-                );
-    }
-}
+//package ru.yandex.practicum;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.jdbc.core.JdbcTemplate;
+//import ru.yandex.practicum.filmorate.Model.User;
+//import ru.yandex.practicum.filmorate.Storage.UserDbStorage;
+//
+//import java.util.Optional;
+//
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+//
+//@SpringBootTest(classes = UserDbStorage.class)
+//@AutoConfigureTestDatabase
+//@RequiredArgsConstructor(onConstructor_ = @Autowired)
+//public class FilmoRateApplicationTests {
+//    private final JdbcTemplate jdbcTemplate = new JdbcTemplate();
+//    private final UserDbStorage userStorage = new UserDbStorage(jdbcTemplate);
+//
+//    @Test
+//    public void testFindUserById() {
+//
+//        Optional<User> userOptional = userStorage.getUserById(1);
+//
+//        assertThat(userOptional)
+//                .isPresent()
+//                .hasValueSatisfying(user ->
+//                        assertThat(user).hasFieldOrPropertyWithValue("id", 1)
+//                );
+//    }
+//}
